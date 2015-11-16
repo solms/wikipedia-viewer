1ff85cdac82e2f9056ef1666422d3f6a4e8749c8
Stykes <solmsd@gmail.com>
2015-11-16 14:06:44 +0900

    Search div moves up and down nicely

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,8 +1,31 @@
+var first = true;
 $(document).ready(function(){
 	var URL_endpoint = 'https://en.wikipedia.org/w/api.php?';
 	// React to changes in the search box
-	$('#search-box').on('input', function(){
-		var URL = URL_endpoint+'action=query&titles='+$(this).val();
-		console.log(URL);
+	$('#search').on('input', function(){
+		if(first){
+			$("#search-div").animate({
+		        height: '20px'
+		    }, 1000);
+		    $("#search").animate({
+		        'margin-top' : '0px'
+		    }, 1000);
+		    first = false;
+		}
+		else if($('#search').val() == ''){
+			$("#search-div").animate({
+		        height: '100%'
+		    }, 1000);
+		    $("#search").animate({
+		        'margin-top' : '15%'
+		    }, 1000);
+
+		    first = true;
+		}
+
+		if($('#search').val() !== ''){
+			var URL = URL_endpoint+'action=query&titles='+$(this).val();
+			console.log(URL);
+		}		
 	});
 });
